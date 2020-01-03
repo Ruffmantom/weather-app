a686b37a1facd1794442869a040401a030d5222e
Tom Ruff <ruffmantom@gmail.com>
2020-01-03 13:10:00 -0700

    added js and some styles

--- weatherScript.js
+++ weatherScript.js
@@ -1,6 +1,28 @@
 $(document).ready(function () {
-    // need to prevent default for the form that im using
-    event.preventDefault();
+    var key = "q=";
+    var apiKey = "7061f9efd83e1dbb05cd692ec2f22061";
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecastAPPID=" + apiKey;
+
+
+    $('#search-btn').on("click", function () {
+        console.log('worked')
+        // need to prevent default for the form that im using
+        event.preventDefault();
+        var searchedCity = $("#city-search").val();
+        key += searchedCity;
+        console.log(key);
+        // key is now concatinated with the searched city on click
+        // need to attatch to the url now
+        queryURL += key;
+        console.log(queryURL)
+        // $.ajax({
+        //     url: queryURL,
+        //     method: "GET"
+        // }).then(function (response) {
+
+        // });
+
+    })
 
 
 
