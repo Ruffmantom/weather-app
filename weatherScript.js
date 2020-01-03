2fd946041ef98631ca5054e60d916a59ce2489ca
Tom Ruff <ruffmantom@gmail.com>
2020-01-03 09:13:05 -0700

    first commit

---
+++ weatherScript.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    // need to prevent default for the form that im using
+    event.preventDefault();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // these ending braces are for the document.ready
+})
