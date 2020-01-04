319f71464854b9e9768aa6fc0442bebc059fc78b
Tom Ruff <ruffmantom@gmail.com>
2020-01-03 19:32:08 -0700

    added moment js to get current date

--- weatherScript.js
+++ weatherScript.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     var key = "q=";
+    var farenheight = 'units=imperial'
     var apiKey = "7061f9efd83e1dbb05cd692ec2f22061";
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?APPID=" + apiKey + '&';
-
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?APPID=" + apiKey + '&' + farenheight + '&';
+    var currentDate = moment().subtract(10, 'days').calendar();
+    console.log(currentDate)
 
     $('#search-btn').on("click", function () {
         console.log('worked')
@@ -35,6 +37,13 @@ $(document).ready(function () {
             p.text(response.city.name);
             // storing the searched city to a p tag below inside the search div
             $("#serched-cities").prepend(p);
+            //store name of city in the searched-location h3
+            $("#searched-location").text(response.city.name + " (" + currentDate + ")");
+
+            // testing to get temp
+            // what I will need is a loop to go through each day of the week and grab the temp
+            var temp = response.list[0].main.temp;
+            console.log(temp);
 
         });
     }
